5fe22d13cc922135f933142f66c6b777f8ae7f80
Alexandru Neacsu <aluneacsu@gmail.com>
2026-01-14 20:29:26 +0200

    Add aliases for eza commands in .aliases.zsh

---
+++ .aliases.zsh
@@ -0,0 +1,7 @@
+#eza
+alias ld='eza -lDh'
+alias lf='eza -lfh --color=always | grep -v /'
+alias lh='eza -dlh .* --group-directories-first'
+alias ll='eza -alh --group-directories-first --time-style=relative --icons'
+alias ls='eza -lh --git --color=always --group-directories-first --icons --time-style=relative'
+alias lt='eza -alh --sort=modified --time-style=relative
