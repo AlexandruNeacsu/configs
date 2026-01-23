ac4d36804db767dcf2ea9e7a33d453d559d7a12c
Alexandru Neacsu <aluneacsu@gmail.com>
2026-01-23 17:59:18 +0200

    Update .aliases.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -2,6 +2,4 @@
 alias ld='eza -lDh'
 alias lf='eza -lfh --color=always | grep -v /'
 alias lh='eza -dlh .* --group-directories-first'
-alias ll='eza -alh --group-directories-first --time-style=relative --icons'
-alias ls='eza -lh --git --color=always --group-directories-first --icons --time-style=relative'
-alias lt='eza -alh --sort=modified --time-style=relative
+alias lt='eza -TD --color=always --icons=always'
